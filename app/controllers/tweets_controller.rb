abfa9f740a33148a4c2f406eaa9b3187a28c9275
Candy <test@MacBook-Air-Candy-2.local>
2016-05-05 18:19:56 +0900

    edit and delete tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+
 before_action :move_to_index, except: :index
 
   def index
@@ -6,12 +7,29 @@ before_action :move_to_index, except: :index
   end
 
   def new
-    
   end
 
   def create
    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
+
+ def destroy
+      tweet = Tweet.find(params[:id])
+      if tweet.user_id == current_user.id
+        tweet.destroy
+      end
+    end
+
+ def update
+      tweet = Tweet.find(params[:id])
+      if tweet.user_id == current_user.id
+        tweet.update(tweet_params)
+      end
+    end
+
+ def edit
+      @tweet = Tweet.find(params[:id])
+    end
 
 private
 def tweet_params
@@ -21,5 +39,6 @@ end
 def move_to_index
   redirect_to action: :index unless user_signed_in?
 end
-
 end
+
+

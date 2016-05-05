99ef09414194584e91522e2c208aa0b36ff4bce7
Candy <test@MacBook-Air-Candy-2.local>
2016-05-05 14:55:55 +0900

    new tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,4 +7,15 @@ class TweetsController < ApplicationController
   def new
     
   end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+private
+def tweet_params
+  params.permit(:name, :image, :text)
+  
+end
+
 end
